0f3add07bed27132e4f5b4b8448d2dbb2e71f30a
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-07 17:32:43 +0600

    "Base Index Page Design"

---
+++ AssetManagementUI/Controllers/AssetEntrysController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetManagementUI.Controllers
+{
+    public class AssetEntrysController : Controller
+    {
+        // GET: AssetEntrys
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AssetManagementUI/Controllers/CheckOutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetManagementUI.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
