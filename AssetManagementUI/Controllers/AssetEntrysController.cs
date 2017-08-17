38aa0d81817c096505db2f58619522c10b82f8fd
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-17 22:28:21 +0600

    "Create Asset Entry Form"

--- AssetManagementUI/Controllers/AssetEntrysController.cs
+++ AssetManagementUI/Controllers/AssetEntrysController.cs
@@ -9,5 +9,10 @@ namespace AssetManagementUI.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
