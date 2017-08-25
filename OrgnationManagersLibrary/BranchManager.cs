4fc6fbb01faf0b2148b432a1a822f68835fbe478
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-25 15:38:05 +0600

    "Create Branch Manager"

---
+++ OrgnationManagersLibrary/BranchManager.cs
@@ -0,0 +1,51 @@
+﻿using OrganizationModelsLibrary;
+using OrgnationGetwaysLibrary;
+using System.Collections.Generic;
+
+namespace OrgnationManagersLibrary
+{
+    public class BranchManager
+    {
+        private readonly BranchGetway _branchGetway = new BranchGetway();
+
+        public int Save(Branch aBranch)
+        {
+            return _branchGetway.Save(aBranch);
+        }
+
+        public int Update(Branch aBranch)
+        {
+            return _branchGetway.Update(aBranch);
+        }
+
+        public Branch FindBranchById(int? id)
+        {
+            return _branchGetway.FindBranchById(id);
+        }
+
+        public Branch FindBranchByName(string name)
+        {
+            return _branchGetway.FindBranchByName(name);
+        }
+
+        public Branch FindBranchByShortName(string shortName)
+        {
+            return _branchGetway.FindBranchByShortName(shortName);
+        }
+
+        public List<Branch> GetBranchByNameOrShortName(string searchInput)
+        {
+            return _branchGetway.GetBranchByNameOrShortName(searchInput);
+        }
+
+        public List<Branch> GetBranchesByOrganizationId(int? organizationId)
+        {
+            return _branchGetway.GetBranchesByOrganizationId(organizationId);
+        }
+
+        public List<Branch> GetAllBRanches()
+        {
+            return _branchGetway.GetAllBRanches();
+        }
+    }
+}
