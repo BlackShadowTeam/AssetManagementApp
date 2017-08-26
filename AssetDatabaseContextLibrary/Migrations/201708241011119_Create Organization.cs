852b6bb12b18616796512d970b4df7442e9d8d65
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-26 13:49:58 +0600

    "Update"

---
+++ AssetDatabaseContextLibrary/Migrations/201708241011119_Create Organization.cs
@@ -0,0 +1,29 @@
+namespace AssetDatabaseContextLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateOrganization : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Organizations",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        ShortName = c.String(nullable: false),
+                        Location = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Organizations");
+        }
+    }
+}
---
+++ AssetDatabaseContextLibrary/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace AssetDatabaseContextLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AssetDatabaseContextLibrary.DatabaseContexts.AssetDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AssetDatabaseContextLibrary.DatabaseContexts.AssetDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
