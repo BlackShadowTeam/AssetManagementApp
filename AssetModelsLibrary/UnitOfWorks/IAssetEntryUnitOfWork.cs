7bbc5b9cbf3e5d2ad08a2e0955dee2bec16d158a
forhadparvez <forhadparvezfahim@gmail.com>
2017-08-28 13:21:22 +0600

    Work Asset Infrastucture

---
+++ AssetModelsLibrary/UnitOfWorks/IAssetEntryUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using AssetModelsLibrary.Repositorys.AssetEntrys;
+using Core.Repository.Library.UnitOfWork;
+
+namespace AssetModelsLibrary.UnitOfWorks
+{
+    public interface IAssetEntryUnitOfWork : IUnitOfWork
+    {
+        IAssetEntryRepository AssetEntry { get; set; }
+        IAttchmentRepository Attchment { get; set; }
+        IFinanceRepository Finance { get; set; }
+        INoteRepository Note { get; set; }
+    }
+}
---
+++ AssetModelsLibrary/UnitOfWorks/IAssetSetupUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using AssetModelsLibrary.Repositorys.AssetSetup;
+using Core.Repository.Library.UnitOfWork;
+
+namespace AssetModelsLibrary.UnitOfWorks
+{
+    public interface IAssetSetupUnitOfWork : IUnitOfWork
+    {
+        IAssetGroupRepository AssetGroup { get; set; }
+        IAssetLocationRepository AssetLocation { get; set; }
+        IAssetManufacturerRepository AssetManufacturer { get; set; }
+        IAssetModelRepository AssetModel { get; set; }
+        IAssetTypeRepository AssetType { get; set; }
+    }
+}
